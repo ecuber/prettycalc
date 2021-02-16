dd6af284bd3e52eef0ae3814131bf71a82285c2c
ecuber <37519236+ecuber@users.noreply.github.com>
2021-02-16 01:21:30 -0500

    Check if expression can be evaluated

--- src/Components/EquationEditor.tsx
+++ src/Components/EquationEditor.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { StaticMathField, EditableMathField, addStyles } from 'react-mathquill'
+import evaluatex from 'evaluatex/dist/evaluatex'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
@@ -7,6 +8,7 @@ addStyles()
 
 const EquationEditor: React.FC = () => {
   const [equation, setEquation] = useState('\\frac{x}{x^2+y^2}')
+  const [valid, setValid] = useState(true)
   return (
     <Row className='justify-content-center align-items-center'>
       <Col lg='auto'>
@@ -17,6 +19,13 @@ const EquationEditor: React.FC = () => {
           className='input'
           latex={equation}
           onChange={(mathField) => {
+            let isValid: boolean = true
+            try {
+              evaluatex(mathField.latex(), { x: 0, y: 0 }, { latex: true })()
+            } catch (e) {
+              isValid = false
+            }
+            setValid(isValid)
             setEquation(mathField.latex())
           }}
           config={{
