dc7baae4a2c952ad680d32683d69b5a087706c53
Elijah Sippel <37519236+ecuber@users.noreply.github.com>
2021-03-29 15:08:50 -0400

    Remove mystery directory
    Don't know where this came from-- I renamed /Components to /components and github didn't remove them apparently

--- src/Components/visly/__folders__.js
+++
@@ -1,3 +0,0 @@
-import * as StarterApp from "./StarterApp";
-
-export { StarterApp };
--- src/Components/visly/builtins/MenuTrigger.d.ts
+++
@@ -1,2 +0,0 @@
-import { PopoverTriggerProps } from './PopoverTrigger';
-export declare function MenuTrigger(props: PopoverTriggerProps): any;
