bd06f51982415e20b669ff6f4de625e780c2df8c
ecuber <37519236+ecuber@users.noreply.github.com>
2021-04-01 14:57:58 -0400

    [Site] Configure Gatsby Cloud hosting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,19 @@
 module.exports = {
-  plugins: ['@chakra-ui/gatsby-plugin', 'gatsby-plugin-react-helmet', 'gatsby-plugin-emotion', {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: 'Elijah\'s Math',
-      short_name: 'elijahmath',
-      start_url: '/',
-      display: 'standalone',
-      icon: 'src/images/icon.png',
-      crossOrigin: 'use-credentials'
+  plugins: [
+    '@chakra-ui/gatsby-plugin',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Elijah\'s Math',
+        short_name: 'elijahmath',
+        start_url: '/',
+        display: 'standalone',
+        icon: 'src/images/icon.png',
+        crossOrigin: 'use-credentials'
+      }
     }
-  }]
+  ]
 }
